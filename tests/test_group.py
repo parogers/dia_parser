704aa87b25073da99029ddcf619e14330bdca13c
Peter Rogers <peter.rogers@gmail.com>
2020-09-11 22:53:04 -0400

    Add test for assigning parent node

--- tests/test_group.py
+++ tests/test_group.py
@@ -17,3 +17,21 @@ def test_group_iterates_all_objects():
     ])
 
     assert list(group.iter_objects()) == [obj1, obj2, obj3]
+
+def test_it_assigns_the_parent_node():
+    obj1 = Object()
+    obj2 = Object()
+    obj3 = Object()
+    group2 = Group([
+        obj3,
+    ])
+    group1 = Group([
+        obj1,
+        obj2,
+        group2,
+    ])
+
+    assert obj1.parent == group1
+    assert obj2.parent == group1
+    assert group2.parent == group1
+    assert obj3.parent == group2
