974b02b0d4b9876f7a450dc613c2304cb7c65302
Peter Rogers <peter.rogers@gmail.com>
2020-09-18 22:25:43 -0400

    Add script to autogenerate API docs

---
+++ tools/generate_api_doc.py
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+(cd src && pydoc-markdown -m dia_parser/parse -m dia_parser/diagram -m dia_parser/obj -m dia_parser/layer -m dia_parser/attributes '{
+    renderer: {
+      type: markdown,
+      descriptive_class_title: false,
+      render_toc: true,
+    }
+}') > docs/API.md
